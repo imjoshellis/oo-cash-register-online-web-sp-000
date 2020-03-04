34ec3b9de29908880930ce6a7c8327e9df047383
Josh Ellis <joshe523@gmail.com>
2020-03-04 15:36:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     @total += price * quantity
-    @items << title if !@items.include?(title)
+    quantity.times { @items << title }
   end
 
   def apply_discount
