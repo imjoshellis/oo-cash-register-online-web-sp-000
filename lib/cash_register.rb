ff8217c5350a7bf57a4aac4434200279606b4d0e
Josh Ellis <joshe523@gmail.com>
2020-03-04 15:34:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,15 +4,21 @@ class CashRegister
   def initialize(discount=nil)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(title, price, quantity=1)
     @total += price * quantity
+    @items < title
   end
 
   def apply_discount
     return "There is no discount to apply." if discount == nil
     @total = @total * (100 - discount) / 100
     return "After the discount, the total comes to $#{@total}."
+  end
+
+  def items
+    @items
   end
 end
