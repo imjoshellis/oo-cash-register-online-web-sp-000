8c52486b5a61c3dd9c07943acd577d64b1abe26e
Josh Ellis <joshe523@gmail.com>
2020-03-04 15:33:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount
-    return "There is no discount to apply." if discount = nil
+    return "There is no discount to apply." if discount == nil
     @total = @total * (100 - discount) / 100
     return "After the discount, the total comes to $#{@total}."
   end
